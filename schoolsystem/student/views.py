b4261d6e828fe85a02cf7f5eccf329ccbf0eab03
sindetisarah <sindetisara@gmail.com>
2021-07-23 18:29:13 +0300

    created a student registration form

--- schoolsystem/student/views.py
+++ schoolsystem/student/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import StudentRegistrationForm
 
-# Create your views here.
+def register_student(request):
+    form=StudentRegistrationForm()
+    return render(request,"register_student.html",{
+        "form":form
+    })
+
+
+
